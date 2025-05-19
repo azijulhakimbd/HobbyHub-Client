6ba71b4a265e6e5f31f322e71ee2e963cad43bb6
azijulhakimbd <azijulhakim.sherpur@gmail.com>
2025-05-19 22:49:24 +0600

    React Router Setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { createBrowserRouter } from "react-router";
+import Root from "../Root/Root";
+import HomeLayout from "../Pages/HomeLayout";
+import AllGroup from "../Pages/AllGroup";
+import CreateGroup from "../Pages/CreateGroup";
+import MyGroups from "../Pages/MyGroups";
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
+import Home from "../Components/Home";
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component:Root,
+    children:[
+        {
+            index:true,
+            Component:HomeLayout,
+        },
+        {
+            path:'/home',
+            Component:Home
+        },
+        {
+            path:'/allgroup',
+            Component:AllGroup,
+
+        },
+        {
+            path:'/creategroup',
+            Component:CreateGroup,
+        },
+        {
+            path:'/mygroups',
+            Component:MyGroups,
+        },
+        {
+            path:'/login',
+            Component:Login
+        },
+        {
+            path:'/register',
+            Component:Register
+        }
+    ]
+  },
+]);
